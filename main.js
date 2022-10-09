e3627e0a4a0cc649c11b2c5b34ce330ae27c648d
Moiwa <38554511+samuelmoiwa@users.noreply.github.com>
2022-10-09 07:03:51 +0000

    update main.js

--- main.js
+++ main.js
@@ -1,47 +1,45 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-undef */
 
-import time from "./modules/dateTime.js";
+import time from './modules/dateTime.js';
 
-const inputBookTitle = document.getElementById("book_title");
-const inputBookAuthor = document.getElementById("book_author");
-const errorMessage = document.querySelector('.error_message')
+const inputBookTitle = document.getElementById('book_title');
+const inputBookAuthor = document.getElementById('book_author');
 
-const addBookBtn = document.querySelector(".btn");
+const addBookBtn = document.querySelector('.btn');
 
 let taskList = [];
-  
- addBookBtn.addEventListener("click", () => {
+
+addBookBtn.addEventListener('click', () => {
   // eslint-disable-next-line eqeqeq
   if (inputBookTitle.value.trim() != 0 && inputBookAuthor.value.trim() != 0) {
-    const localItems = JSON.parse(localStorage.getItem("localItem"));
+    const localItems = JSON.parse(localStorage.getItem('localItem'));
     if (localItems === null) {
       taskList = [];
     } else {
       taskList = localItems;
     }
     taskList.push(`${inputBookTitle.value} : ${inputBookAuthor.value}`);
-    localStorage.setItem("localItem", JSON.stringify(taskList));
-    inputBookAuthor.value = "";
-    inputBookTitle.value = "";
+    localStorage.setItem('localItem', JSON.stringify(taskList));
+    inputBookAuthor.value = '';
+    inputBookTitle.value = '';
     ClassShowListItems.showItem();
   }
 });
-  
 
 class ClassShowListItems {
   static showItem() {
-  let localItems = JSON.parse(localStorage.getItem("localItem"));
-  if (localItems === null) {
-    taskList = [];
-  } else {
-    taskList = localItems;
-  }
+    const localItems = JSON.parse(localStorage.getItem('localItem'));
+    if (localItems === null) {
+      taskList = [];
+    } else {
+      taskList = localItems;
+    }
 
-  let html = "";
-  const itemShow = document.querySelector(".bookLists");
-  taskList.forEach((bookTitle, index) => {
-    html += `
+    let html = '';
+    const itemShow = document.querySelector('.bookLists');
+    taskList.forEach((bookTitle, index) => {
+      html += `
     <div class="todoListItems">
       <div class="bookList">
         <p class="titleText">${bookTitle}</p>
@@ -49,59 +47,59 @@ class ClassShowListItems {
       </div>
     </div>
     `;
-  });
-  itemShow.innerHTML = html;
-}
+    });
+    itemShow.innerHTML = html;
+  }
 }
 ClassShowListItems.showItem();
 
 function deleteItem(i) {
-  let localItems = JSON.parse(localStorage.getItem("localItem"));
+  const localItems = JSON.parse(localStorage.getItem('localItem'));
   localItems.splice(i, 1);
-  localStorage.setItem("localItem", JSON.stringify(localItems));
+  localStorage.setItem('localItem', JSON.stringify(localItems));
   ClassShowListItems.showItem();
 }
 
-const deleteTaskBtns = document.querySelectorAll(".deleteTask");
+const deleteTaskBtns = document.querySelectorAll('.deleteTask');
 
 deleteTaskBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const i = e.currentTarget.getAttribute("data-index");
+  btn.addEventListener('click', (e) => {
+    const i = e.currentTarget.getAttribute('data-index');
     deleteItem(i);
     window.location.reload();
   });
 });
 
-const navList = document.querySelector("#nav-list");
-const navAddNew = document.querySelector("#nav-add-new");
-const navContact = document.querySelector("#nav-contact");
+const navList = document.querySelector('#nav-list');
+const navAddNew = document.querySelector('#nav-add-new');
+const navContact = document.querySelector('#nav-contact');
 
-const bookListSection = document.querySelector(".books_list_main_div");
-const addNewSection = document.querySelector(".input_value");
-const contactSection = document.querySelector(".contact_info");
+const bookListSection = document.querySelector('.books_list_main_div');
+const addNewSection = document.querySelector('.input_value');
+const contactSection = document.querySelector('.contact_info');
 
-navList.addEventListener("click", () => {
+navList.addEventListener('click', () => {
   if (navList) {
-    bookListSection.style.display = "flex";
-    addNewSection.style.display = "none";
-    contactSection.style.display = "none";
+    bookListSection.style.display = 'flex';
+    addNewSection.style.display = 'none';
+    contactSection.style.display = 'none';
     window.location.reload();
   }
 });
 
-navAddNew.addEventListener("click", () => {
+navAddNew.addEventListener('click', () => {
   if (navList) {
-    bookListSection.style.display = "none";
-    addNewSection.style.display = "flex";
-    contactSection.style.display = "none";
+    bookListSection.style.display = 'none';
+    addNewSection.style.display = 'flex';
+    contactSection.style.display = 'none';
   }
 });
 
-navContact.addEventListener("click", () => {
+navContact.addEventListener('click', () => {
   if (navList) {
-    bookListSection.style.display = "none";
-    addNewSection.style.display = "none";
-    contactSection.style.display = "flex";
+    bookListSection.style.display = 'none';
+    addNewSection.style.display = 'none';
+    contactSection.style.display = 'flex';
   }
 });
 
